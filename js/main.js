d5e85aa9182177f3ce711fe9b8d332f821ed6648
valeria caramellino <valeriacaramellino@gmail.com>
2023-04-19 15:34:39 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+let container = document.createElement("div");
+container.id = "container";
+document.body.append(container);
+
+let box = document.createElement("div");
+box.id = "box";
+container.append(box);
+
+let card0= document.createElement("img");
+card0.className = "card";
+card0.src= "01.wepb";
+box.append(card0);
+
