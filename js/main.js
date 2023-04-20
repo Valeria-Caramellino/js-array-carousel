d2ab7dce61e4d302de69930dcae1c07c8caadf70
valeria caramellino <valeriacaramellino@gmail.com>
2023-04-20 15:34:21 +0200

    feat(const card + rotazione infinita )

--- js/main.js
+++ js/main.js
@@ -7,33 +7,6 @@ let box = document.createElement("div");
 box.id = "box";
 container.append(box);
 
-
-//creo le card
-let card0= document.createElement("img");
-card0.className = "card";
-card0.src= "img/01.webp";
-box.append(card0);
-
-let card1= document.createElement("img");
-card1.className = "card hidden";
-card1.src="img/02.webp";
-box.append(card1);
-
-let card2= document.createElement("img");
-card2.className = "card hidden";
-card2.src="img/03.webp";
-box.append(card2);
-
-let card3= document.createElement("img");
-card3.className = "card hidden";
-card3.src="img/04.webp";
-box.append(card3);
-
-let card4= document.createElement("img");
-card4.className = "card hidden";
-card4.src="img/05.webp";
-box.append(card4);
-
 //creazione bottoni
 let buttonGo = document.createElement ("button");
 buttonGo.type = buttonGo;
@@ -47,45 +20,50 @@ buttonBeck.innerHTML = '▼';
 buttonBeck.className = "btnIndietro btnStyle";
 box.append(buttonBeck);
 
+
+//creo le card
+const immagini = [
+    "img/01.webp",
+    "img/02.webp",
+    "img/03.webp",
+    "img/04.webp",
+    "img/05.webp",
+]
+
+const insiemeImmagini = document.getElementById("box");
+
+for (let i = 0; i < immagini.length; i++) {
+    const risultato = immagini[i];
+    const img = document.createElement("img");
+    img.src = risultato;
+    
+    if ( i === 0 ){
+        img.classList.add("visibile");
+    }
+    box.append(img);
+}
+
+
 //raggruppo le card 
-let cardElements = document.getElementsByClassName("card");
+const imgContainer = document.querySelectorAll("#box > img");
 let inizioCard = 0;
 
 //bottone avanti
 buttonGo.addEventListener("click", function(){
-    if(inizioCard < 4) {
-        for (let c = 0; c < cardElements.length; c++) {
-            const carte = cardElements[c];
-
-            if(c == inizioCard+1 ) {
-                carte.classList.remove("hidden");
-            } else {
-                carte.classList.add("hidden");  
-            }
-        }
-        inizioCard++;
-    } else {
-        console.log("non c'è una slide successiva");
+    imgContainer[inizioCard].classList.remove("visibile");
+    inizioCard++;
+    if ( inizioCard == imgContainer.length ){
+        inizioCard= 0;
     }
+    imgContainer[inizioCard].classList.add("visibile");
 });
 
 //bottone indietro
 buttonBeck.addEventListener("click", function(){
-
-    if(inizioCard>0) {
-        for (let c = 0; c < cardElements.length; c++) {
-                const carte = cardElements[c];
-
-            if(c == inizioCard-1 ) {
-                carte.classList.remove("hidden");
-            } else {
-                carte.classList.add("hidden");
-            }
-        }
-        
-        inizioCard--;
-    } else {
-        console.log("non c'è una slide precedente");
+    imgContainer[inizioCard].classList.remove("visibile");
+    inizioCard--;
+    if ( inizioCard < 0 ){
+        inizioCard= imgContainer.length -1;
     }
-
+    imgContainer[inizioCard].classList.add("visibile");
 });
